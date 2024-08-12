a37c4f434a4a17792de384723d491f9b1982cf00
provincialcxz <eremamihaylov@mail.ru>
2024-08-12 14:23:36 +0300

    feat: added parallel task execution

--- SshScanner/SshScanner/Program.cs
+++ SshScanner/SshScanner/Program.cs
@@ -1,59 +1,83 @@
-﻿using System.Net.NetworkInformation;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 using Renci.SshNet;
 
 namespace SshScanner
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Console.WriteLine("  _____     _      _____                                 \r\n / ____|   | |    / ____|                                \r\n| (___  ___| |__ | (___   ___ __ _ _ __  _ __   ___ _ __ \r\n \\___ \\/ __| '_ \\ \\___ \\ / __/ _` | '_ \\| '_ \\ / _ \\ '__|\r\n ____) \\__ \\ | | |____) | (_| (_| | | | | | | |  __/ |   \r\n|_____/|___/_| |_|_____/ \\___\\__,_|_| |_|_| |_|\\___|_|   \nmade by provincialcxz\n\n");
+
             string username = "your_username";
             string password = "your_password";
-            string baseIp = "192.168.1."; // ваша подсеть
+            string baseIp = "192.168.1.";
 
             List<string> iptrue = new List<string>();
+
+            var tasks = new List<Task>();
+
+            Console.WriteLine("Start scanning...");
 
             for (int i = 1; i < 255; i++)
             {
                 string ip = baseIp + i.ToString();
 
-                if (PingHost(ip))
+                tasks.Add(Task.Run(async () =>
                 {
-                    Console.WriteLine($"IP {ip} is up. Trying SSH connection...");
-
-                    if (TrySshConnection(ip, username, password))
+                    if (await PingHostAsync(ip))
                     {
-                        iptrue.Add(ip);
-                        Console.WriteLine($"IP {ip} ---------------> TRUE");
+                        Console.WriteLine($"IP {ip} is up. Trying SSH connection...");
+
+                        if (await TrySshConnectionAsync(ip, username, password))
+                        {
+                            lock (iptrue)
+                            {
+                                iptrue.Add(ip);
+                            }
+                            Console.WriteLine($"IP {ip} ---------------> TRUE");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"IP {ip} - false");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine($"IP {ip} - false");
+                        Console.WriteLine($"IP {ip} is down.");
                     }
-                }
-                else
-                {
-                    Console.WriteLine($"IP {ip} is down.");
-                }
+                }));
             }
+
+            await Task.WhenAll(tasks);
 
             Console.WriteLine("Scan completed.\n");
 
             Console.WriteLine("Successful IPs:");
-            foreach (string ip in iptrue)
+            if (iptrue.Count == 0)
             {
-                Console.WriteLine(ip);
+                Console.WriteLine("N/A");
+            }
+            else
+            {
+                foreach (string ip in iptrue)
+                {
+                    Console.WriteLine(ip);
+                }
             }
         }
 
-        static bool PingHost(string nameOrAddress)
+        static async Task<bool> PingHostAsync(string nameOrAddress)
         {
             bool pingable = false;
             Ping pinger = new Ping();
 
             try
             {
-                PingReply reply = pinger.Send(nameOrAddress);
+                PingReply reply = await Task.Run(() => pinger.Send(nameOrAddress));
                 pingable = reply.Status == IPStatus.Success;
             }
             catch (PingException)
@@ -64,22 +88,25 @@ namespace SshScanner
             return pingable;
         }
 
-        static bool TrySshConnection(string ip, string username, string password)
+        static Task<bool> TrySshConnectionAsync(string ip, string username, string password)
         {
-            try
+            return Task.Run(() =>
             {
-                using (var client = new SshClient(ip, username, password))
+                try
                 {
-                    client.Connect();
-                    client.Disconnect();
+                    using (var client = new SshClient(ip, username, password))
+                    {
+                        client.Connect();
+                        client.Disconnect();
+                    }
+                    return true;
                 }
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            });
         }
     }
 }
